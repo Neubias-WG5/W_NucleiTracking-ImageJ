dca5f573393480fc3eec93ed74c6337b573eb93e
volker-baecker <volker.baecker@mri.cnrs.fr>
2019-01-14 18:42:16 +0100

    wrapper complete - uploads tracks as annotations on every slide.

--- wrapper.py
+++ wrapper.py
@@ -3,12 +3,16 @@ import sys
 from subprocess import call
 
 from cytomine import CytomineJob
-from cytomine.models import Annotation, Job, ImageInstanceCollection, AnnotationCollection, Property, ImageGroupCollection
+from cytomine.models import Annotation, Job, ImageInstanceCollection, AnnotationCollection, Property, ImageGroupCollection, ImageInstance, ImageSequenceCollection
 from shapely.affinity import affine_transform
+from shapely.geometry import Polygon, MultiPolygon, Point, LineString
 from skimage import io
-
-from annotation_exporter import mask_to_objects_3d
+from skimage.measure import points_in_poly, label as label_fn
+from annotation_exporter import mask_to_points_2d, AnnotationSlice
 from neubiaswg5.metrics import computemetrics_batch
+from itertools import groupby
+from shapely.geometry import LineString
+import numpy as np
 
 def main(argv):
     # 0. Initialize Cytomine client and job
@@ -64,30 +68,48 @@ def main(argv):
             err_desc = "Failed to execute the ImageJ macro (return code: {})".format(return_code)
             cj.job.update(progress=50, statusComment=err_desc)
             raise ValueError(err_desc)
+	cj.job.update(progress=51, statusComment="Workflow finished...")
 
         # 4. Upload the annotation and labels to Cytomine (annotations are extracted from the mask using
         # the AnnotationExporter module)
-        '''        for image in cj.monitor(input_images, start=60, end=80, period=0.1, prefix="Extracting and uploading polygons from masks"):
-            file = "{}.tif".format(image.id)
-            path = os.path.join(out_path, file)
-            data = io.imread(path)
+	for image in cj.monitor(input_images, start=60, end=80, period=0.1, prefix="Extracting and uploading tracks from point-masks"):
+	    file = "{}.tif".format(image.id)
+	    path = os.path.join(out_path, file)
+	    data = io.imread(path)
 
-            # extract objects
-            slices = mask_to_objects_3d(data)
+	    # extract objects
+	    objects = mask_to_points_3d(data, time=True)
 
-            print("Found {} polygons in this image {}.".format(len(slices), image.id))
+	    objects = sorted(objects, key=lambda annotationSlice: annotationSlice.time)    
+	    objects = sorted(objects, key=lambda annotationSlice: annotationSlice.label)    
+	    grouped = groupby(objects,  key=lambda annotationSlice: annotationSlice.label)
+	    
+	    image_sequences = ImageSequenceCollection().fetch_with_filter("imagegroup", image.id)
+	    
+	    depth_to_image = {iseq.channel: iseq.image for iseq in image_sequences}
+	    height = ImageInstance().fetch(image_sequences[0].image).height
+	    
+	    collection = AnnotationCollection()
+	    
+	    groups = []
+	    uniquekeys = []
+	    for k, g in grouped:
+		groups.append(list(g))      # Store group iterator as a list
+		uniquekeys.append(k)
+	    
+	    for key, group in zip(uniquekeys, groups):
+		listOfPoints = []
+		for oSlice in group:
+		    listOfPoints.append((oSlice.polygon.x, oSlice.polygon.y))
+		shape = LineString(listOfPoints)
+		for oSlice2 in group:
+		    polygon = affine_transform(shape, [1, 0, 0, -1, 0, height])
+		    annotation = Annotation(location=polygon.wkt, id_image=depth_to_image[oSlice2.time], id_project=cj.parameters.cytomine_id_project, property=[{"key": "index", "value": str(oSlice2.label)}])
+		    collection.append(annotation)
+        
+	    collection.save()
+    	    print("Found {} objects in this image {}.".format(len(objects), image.id))
 
-            # upload
-            collection = AnnotationCollection()
-            for obj_slice in slices:
-                collection.append(Annotation(
-                    location=affine_transform(obj_slice.polygon, [1, 0, 0, -1, 0, image.height]).wkt,
-                    id_image=image.id, id_project=cj.parameters.cytomine_id_project, property=[
-                        {"key": "index", "value": str(obj_slice.label)}
-                    ]
-                ))
-            collection.save()
-        '''
         # 5. Compute and upload the metrics
         cj.job.update(progress=80, statusComment="Computing and uploading metrics...")
         outfiles, reffiles = zip(*[
@@ -102,8 +124,28 @@ def main(argv):
             Property(cj.job, key=key, value=str(value)).save()
         Property(cj.job, key="IMAGE_INSTANCES", value=str([im.id for im in input_images])).save()
 
-        # 6. End
-        cj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
+	# 6. Cleanup
+	for image in input_images:
+	    file = str(image.id)
+	    path = out_path + "/" + file + ".tif"
+	    os.remove(path);
+	    path = in_path + "/" + file  + ".tif"
+	    os.remove(path);
+	    path = gt_path + "/" + file  + ".tif"
+	    os.remove(path);
+	    
+	tmpData = ['intracks.xml', 'intracks.xml.score.txt', 'reftracks.xml']
+	for file in tmpData:
+	    path = tmp_path + "/" + file
+	    os.remove(path);
+	if os.path.exists(working_path):
+	    os.rmdir(in_path)
+	    os.rmdir(out_path)
+	    os.rmdir(gt_path)
+	    os.rmdir(tmp_path)
+	    os.rmdir(working_path)
+		# 7. End
+		cj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
 
 
 if __name__ == "__main__":
